a26bab2c02343edd4611dec5315727761271c603
dayDream001 <2212472990@qq.com>
2022-03-26 18:20:05 +0800

    NoteVueDemo

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>头部展示</div>
+  <div class="header">
+    <div class="collapse-btn">
+      <i class="el-icon-menu"/>
+    </div>
+  </div>
 </template>
 <script >
 export default {
 
 }
 </script>
-<style></style>
+<style scoped>
+  .header{
+    position: relative;
+    background-color: #253041;
+    box-sizing: border-box;
+    width: 100%;
+    height: 70px;
+    font-size: 22px;
+    color: #ffffff;
+  }
+  .collapse-btn{
+    float: left;
+    padding: 0 21px;
+    cursor: pointer;
+    line-height: 70px;
+  }
+</style>
